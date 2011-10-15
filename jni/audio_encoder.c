d3d5eedc09e71173de844398e6138f872ed5511a
Ching Yi, Chan <chingyichan.tw@gmail.com>
2011-10-15 11:16:55 +0800

    implement the allocate and destroy functions.

--- jni/audio_encoder.c
+++ jni/audio_encoder.c
@@ -20,8 +20,60 @@ struct lame_ctx
 JNIEXPORT jboolean JNICALL Java_jug_ndk_audio_encoder_AudioEncoder_allocate
 (JNIEnv *env, jobject thiz, jint channels, jint bytesInSample, jint sampleRate)
 {
-    info("invoke allocate");
-    return JNI_FALSE;
+    jclass cls = (*env)->GetObjectClass(env, thiz);
+    jfieldID lame_pointer_field = (*env)->GetFieldID(env, cls, "nativePointerAddress", "I");
+    int my_lame_addr = (*env)->GetIntField(env, thiz, lame_pointer_field);
+    if(my_lame_addr != 0)
+    {
+        info("native lame address is not null.");
+        return JNI_FALSE;
+    }
+
+    lame_global_flags *gfp = NULL;
+    gfp = lame_init();
+    if(gfp == NULL)
+    {
+        info("cannot allocate lame");
+        return JNI_FALSE;
+    }
+
+    jfieldID out_samplerate_field = (*env)->GetFieldID(env, cls, "outSampleRate", "I");
+    jfieldID out_bitrate_field = (*env)->GetFieldID(env, cls, "outBitRate", "I");
+    jfieldID out_mono_field = (*env)->GetFieldID(env, cls, "outMono", "I");
+    int out_samplerate = (*env)->GetIntField(env, thiz, out_samplerate_field);
+    int out_bitrate = (*env)->GetIntField(env, thiz, out_bitrate_field);
+    int out_mono = (*env)->GetIntField(env, thiz, out_mono_field);
+
+    info("init lame_global_flags: %p", gfp);
+    lame_set_num_channels(gfp, channels);
+    lame_set_in_samplerate(gfp, sampleRate);
+    lame_set_out_samplerate(gfp, out_samplerate);
+    lame_set_brate(gfp, out_bitrate);
+    lame_set_mode(gfp, out_mono == 1 ? 3 : 0 );
+    lame_set_quality(gfp, 7); /* 2=high  5 = medium  7=low */ 
+
+    info("out_sample_rate: %d", out_samplerate);
+    info("out_bitrate: %d", out_bitrate);
+    info("out_mono: %s", out_mono == 1 ? "yes" : "no");
+
+    if(lame_init_params(gfp) <0)
+    {
+        info("init lame_global_flags failure.");
+        lame_close(gfp);
+        return JNI_FALSE;
+    }
+
+    info("lame_init_params is ok.");
+    struct lame_ctx* ctx = malloc(sizeof(struct lame_ctx));
+    memset(ctx, 0, sizeof(struct lame_ctx));
+    ctx->gfp = gfp;
+    ctx->bytes_per_sample = bytesInSample;
+    ctx->channels = channels;
+
+    (*env)->SetIntField(env, thiz, lame_pointer_field, (int) ctx);
+    info("create lame_ctx: %p", ctx);
+    
+    return JNI_TRUE;
 }
 
 /*
@@ -44,6 +96,18 @@ JNIEXPORT jint JNICALL Java_jug_ndk_audio_encoder_AudioEncoder_encode
 JNIEXPORT void JNICALL Java_jug_ndk_audio_encoder_AudioEncoder_destroy
   (JNIEnv *env, jobject thiz)
 {
-    info("invoke destroy");
+    jclass cls = (*env)->GetObjectClass(env, thiz);
+    jfieldID lame_pointer_field = (*env)->GetFieldID(env, cls, "nativePointerAddress", "I");
+    int my_lame_addr = (*env)->GetIntField(env, thiz, lame_pointer_field);
+    if(my_lame_addr != 0)
+    {
+        struct lame_ctx* ctx = (struct lame_ctx*) my_lame_addr;
+        info("close lame global flag: %p", ctx->gfp);
+        lame_close(ctx->gfp);
+
+        info("destroy lame-ctx: %p", ctx);
+        free(ctx);
+        (*env)->SetIntField(env, thiz, lame_pointer_field, 0);
+    }
 }
 
